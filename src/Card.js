16cb6880e700ec0b029a35535555fe0b15ff3f0a
mazba.turza <turza1994@yahoo.com>
2020-11-06 20:00:00 -0600

    cat image height change

--- src/Card.js
+++ src/Card.js
@@ -5,8 +5,8 @@ const Card = ({id, name, origin, desc, image})=>{
         if(!image) return null;
         return(
             <div className="col-md-4">
-                <div className="bg-near-white br3 grow bw-4 shadow-5 w-100 lh-copy">
-                    <img className="w-100 rounded-top" src={image.url} alt='robo friend'/>
+                <div className="bg-near-white br3 grow bw-4 shadow-5 w-100 lh-copy" style={{height: '730px'}}>
+                    <img className="w-100 rounded-top" src={image.url} alt='robo friend' style={{height: '300px'}} />
                     <div className="p-3">
                         <h2 className="text-center mb-3">{name} </h2>
                         <h4>Origin: {origin} </h4>
